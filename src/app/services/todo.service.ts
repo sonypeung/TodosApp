7fd3664c65f98d82f62a48be180b922ae2319e9c
Sony <sony.peung@gmail.com>
2019-05-21 21:19:04 +0200

    added the completeTodo service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -3,16 +3,30 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Todo } from '../models/Todo';
 import { Observable } from 'rxjs';
 
+const httpOptions = {
+  headers : new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
   todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
-
+  urlLimit: string = '?_limit=5';
   constructor(private http: HttpClient) { }
 
+  // Get Todos
   getTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>(this.todosUrl);
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.urlLimit}`);
+    // return this.http.get<Todo[]>(this.todosUrl+this.urlLimit);
+  }
+
+  // Complete a To do
+  completeTodo(todo: Todo): Observable<any>{
+    let url: string = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 }
